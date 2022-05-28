4570001fa172d60a7d1ce731cc3041cef93e6e94
nineya <361654768@qq.com>
2022-05-28 22:20:25 +0800

    chore(npm):npm构建相关适配

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const { version } = require('./package.json')
+const themePath = `/themes/dream/source/lib/halo-comment@${version}/`
+const npmPath = `https://unpkg.com/halo-comment-dream@${version}/dist/`
 
 module.exports = {
   productionSourceMap: false,
-  publicPath: process.env.ASSETS ? `/themes/dream/source/lib/halo-comment@${version}` : '/',
+  publicPath: process.env.ASSETS === 'theme' ? themePath : process.env.ASSETS === 'npm' ? npmPath : '/',
   configureWebpack: process.env.ASSETS
     ? {
         plugins: [
