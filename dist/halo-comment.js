3b2afc13497ee9ea44d89f7caaab2a79fd2f8e8d
nineya <361654768@qq.com>
2022-06-03 00:28:20 +0800

    build(version):版本号更新为1.0.3

--- dist/halo-comment.js
+++ dist/halo-comment.js
@@ -8498,7 +8498,7 @@ function wrap (Vue, Component) {
 
 // EXTERNAL MODULE: ./node_modules/css-loader/dist/runtime/api.js
 var api = __webpack_require__(3645);
-;// CONCATENATED MODULE: ./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/Comment.vue?vue&type=template&id=8a00c3c6&shadow
+;// CONCATENATED MODULE: ./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/Comment.vue?vue&type=template&id=d964f6d0&shadow
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"halo-comment",class:_vm.mergedConfigs.night ? 'night' : '',attrs:{"id":"halo-comment"}},[_c('keep-alive',[_c('comment-editor',{attrs:{"configs":_vm.mergedConfigs,"options":_vm.options,"target":_vm.target,"targetId":_vm.id}})],1),(!_vm.mergedConfigs.autoLoad && !_vm.list.loaded)?_c('div',{staticClass:"load-comment"},[_c('button',{staticClass:"btn btn-primary",attrs:{"type":"button"},on:{"click":_vm.handleGetComments}},[_vm._v("加载评论")])]):_vm._e(),(_vm.list.loaded)?_c('div',{staticClass:"comment-action"},[_c('h3',{staticClass:"comment-title"},[_vm._v(_vm._s(_vm.list.total)+" 条评论")]),_c('svg',{staticClass:"comment-refresh",attrs:{"viewBox":"0 0 1024 1024","xmlns":"http://www.w3.org/2000/svg"},on:{"click":function($event){return _vm.handlePaginationChange(0)}}},[_c('path',{attrs:{"d":"M55.935033 264.48948c0 0 85.897017-132.548409 221.81443-203.673173 135.916406-71.121743 303.368504-50.646859 413.187968 18.319527 109.819465 68.970415 146.791894 127.160016 146.791894 127.160016l94.59499-53.879895c0 0 19.576483-9.697092 19.576483 12.932142l0 338.379961c0 0 0 30.17399-22.837719 19.395191-19.210878-9.062571-226.959086-127.198289-292.424528-164.466828-35.950145-16.035251-4.365101-29.062068-4.365101-29.062068l91.284402-52.173738c0 0-52.068992-65.209619-128.278989-99.744682-81.576231-42.501826-157.948384-47.541735-251.497925-12.224097-61.002644 23.025054-132.823368 81.988166-184.553949 169.082716L55.935033 264.48948 55.935033 264.48948 55.935033 264.48948zM904.056909 711.697844c0 0-85.897017 132.550423-221.816444 203.671159-135.917413 71.12275-303.366489 50.651895-413.186961-18.315498-109.825508-68.972429-146.790886-127.165052-146.790886-127.165052L27.662591 823.768348c0 0-19.572454 9.703135-19.572454-12.932142L8.090137 472.459267c0 0 0-30.170968 22.831676-19.397205 19.211885 9.067607 226.965129 127.198289 292.430571 164.470856 35.950145 16.035251 4.366109 29.058039 4.366109 29.058039l-91.285409 52.175753c0 0 52.071006 65.206598 128.279996 99.744682 81.57321 42.498804 157.942341 47.540728 251.496918 12.222082 60.998616-23.026061 132.820346-81.983131 184.546898-169.082716L904.056909 711.697844 904.056909 711.697844 904.056909 711.697844zM904.056909 711.697844"}})])]):_vm._e(),_c('comment-loading',{directives:[{name:"show",rawName:"v-show",value:(_vm.list.loading),expression:"list.loading"}],attrs:{"configs":_vm.configs}}),(_vm.list.data.length >= 1)?_c('ul',{staticClass:"comment-nodes"},[_vm._l((_vm.list.data),function(comment,index){return [_c('CommentNode',{key:index,attrs:{"comment":comment,"configs":_vm.mergedConfigs,"options":_vm.options,"target":_vm.target,"targetId":_vm.id}})]})],2):_vm._e(),(_vm.list.loaded && !_vm.list.loading && _vm.list.data.length <= 0)?_c('div',{staticClass:"comment-empty"},[_vm._v("暂无评论")]):_vm._e(),(_vm.list.pages > 1)?_c('pagination',{attrs:{"page":_vm.list.params.page,"size":_vm.list.size,"total":_vm.list.total},on:{"change":_vm.handlePaginationChange}}):_vm._e()],1)}
 var staticRenderFns = []
 
@@ -9221,7 +9221,7 @@ class Tokenizer {
         l = item.header.length;
         for (j = 0; j < l; j++) {
           item.header[j].tokens = [];
-          this.lexer.inline(item.header[j].text, item.header[j].tokens);
+          this.lexer.inlineTokens(item.header[j].text, item.header[j].tokens);
         }
 
         // cell child tokens
@@ -9230,7 +9230,7 @@ class Tokenizer {
           row = item.rows[j];
           for (k = 0; k < row.length; k++) {
             row[k].tokens = [];
-            this.lexer.inline(row[k].text, row[k].tokens);
+            this.lexer.inlineTokens(row[k].text, row[k].tokens);
           }
         }
 
@@ -12754,7 +12754,6 @@ const defaultConfig = {
       }
 
       defaultConfig['night'] = localStorage && localStorage.getItem('night') === 'true' || false;
-      console.log(defaultConfig);
       return Object.assign(defaultConfig, externalConfigs);
     }
 
