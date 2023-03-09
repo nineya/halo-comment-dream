22ae0585a228b61153e8a9f852577ec6d7e69bc2
nineya <361654768@qq.com>
2023-03-09 09:12:04 +0800

    perf(token): 实现博主登录失败刷新token

--- src/plugins/admin-client.js
+++ src/plugins/admin-client.js
@@ -1,20 +1,87 @@
-import { AdminApiClient, HaloRestAPIClient } from '@halo-dev/admin-api'
+import { AdminApiClient, Axios, HaloRestAPIClient } from '@halo-dev/admin-api'
+
+// let accessToken = localStorage && localStorage.getItem('HALO__Access-Token')
+// accessToken = accessToken ? JSON.parse(accessToken) : undefined
+// if (accessToken && accessToken.expire > new Date().getTime()) {
+//   accessToken = accessToken['value']['access_token']
+// } else {
+//   accessToken = undefined
+// }
+//
+// //halo http 请求客户端.
+// const haloRestApiClient = new HaloRestAPIClient({
+//   baseUrl: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8090',
+//   auth: { adminToken: accessToken }
+// })
+//
+// // 通过 haloRestApiCLient 创建 adminApiClient。
+// const adminClient = new AdminApiClient(haloRestApiClient)
 
 let accessToken = localStorage && localStorage.getItem('HALO__Access-Token')
 accessToken = accessToken ? JSON.parse(accessToken) : undefined
-if (accessToken && accessToken.expire > new Date().getTime()) {
-  accessToken = accessToken['value']['access_token']
-} else {
-  accessToken = undefined
+
+let adminClient
+
+if (accessToken) {
+  //halo http 请求客户端.
+  const haloRestApiClient = new HaloRestAPIClient({
+    baseUrl: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8090'
+  })
+  // 通过 haloRestApiCLient 创建 adminApiClient。
+  adminClient = new AdminApiClient(haloRestApiClient)
+
+  haloRestApiClient.interceptors.request.use(
+    config => {
+      config.headers['Admin-Authorization'] = accessToken['value']['access_token']
+      return config
+    },
+    error => {
+      return Promise.reject(error)
+    }
+  )
+
+  let isRefreshingToken = false
+  let pendingRequests = []
+
+  haloRestApiClient.interceptors.response.use(
+    response => {
+      return response
+    },
+    async error => {
+      const response = error.response
+      const data = response ? response.data : null
+
+      if (Axios.isCancel(error) || /Network Error/.test(error.message) || !data || data.status !== 401) {
+        return Promise.reject(error)
+      }
+
+      const originalRequest = error.config
+
+      if (isRefreshingToken) {
+        return new Promise(resolve => {
+          pendingRequests.push(() => {
+            resolve(Axios(originalRequest))
+          })
+        })
+      }
+      isRefreshingToken = true
+      try {
+        accessToken['value'] = await adminClient
+          .refreshToken(accessToken['value']['refresh_token'])
+          .then(response => response.data)
+        accessToken.expire = new Date().getTime() + accessToken['value']['expired_in'] * 1000
+        localStorage.setItem('HALO__Access-Token', JSON.stringify(accessToken))
+        pendingRequests.forEach(callback => callback())
+        pendingRequests = []
+
+        return Axios(originalRequest)
+      } catch (e) {
+        return Promise.reject(e)
+      } finally {
+        isRefreshingToken = false
+      }
+    }
+  )
 }
-
-//halo http 请求客户端.
-const haloRestApiClient = new HaloRestAPIClient({
-  baseUrl: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8090',
-  auth: { adminToken: accessToken }
-})
-
-// 通过 haloRestApiCLient 创建 adminApiClient。
-const adminClient = new AdminApiClient(haloRestApiClient)
 
 export default adminClient
